8a4beaa6a11e9bcfed4c0e692c8cf51268e9f27f
Евгений Владимирович Щёголев <shchegolev@ngs.ru>
2020-12-01 17:58:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scheduler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
